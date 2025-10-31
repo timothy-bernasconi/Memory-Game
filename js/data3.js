62677c885608ee8b6026ec84914eaffcd1a33346
Timothy Bernasconi <tim.bernasconi1@gmail.com>
2025-10-31 15:57:44 +0100

    ajout de 2 nouveaux themes, fleurs et aliments

---
+++ js/data3.js
@@ -0,0 +1,23 @@
+export const images = [
+"./assets/aliment1.png",
+"./assets/aliment1.png",
+"./assets/aliment2.png",
+"./assets/aliment2.png",
+"./assets/aliment3.png",
+"./assets/aliment3.png",
+"./assets/aliment4.png",
+"./assets/aliment4.png",
+"./assets/aliment5.png",
+"./assets/aliment5.png",
+"./assets/aliment6.png",
+"./assets/aliment6.png",
+"./assets/aliment7.png",
+"./assets/aliment7.png",
+"./assets/aliment8.png",
+"./assets/aliment8.png",
+"./assets/aliment9.png",
+"./assets/aliment9.png",
+"./assets/aliment10.png",
+"./assets/aliment10.png"
+
+];
---
+++ js/main3.js
@@ -0,0 +1,45 @@
+import { images } from "./data3.js";
+import { melanger } from "./function.js";
+
+const imagesMelangees = melanger(images);
+const grille = document.getElementById("grille");
+
+let cartesRetournees = [];
+
+imagesMelangees.forEach(src => {
+  const carte = document.createElement("div");
+  carte.classList.add("carte");
+
+  const front = document.createElement("div");
+  front.classList.add("front");
+  front.innerHTML = `<img src="./assets/versoBleu.png" alt="versorose">`;
+
+  const back = document.createElement("div");
+  back.classList.add("back");
+  back.innerHTML = `<img src="${src}" alt="recto">`;
+
+  carte.appendChild(front);
+  carte.appendChild(back);
+  grille.appendChild(carte);
+
+  carte.addEventListener("click", () => {
+    if (cartesRetournees.length === 2 || carte.classList.contains("flipped")) return;
+
+    carte.classList.add("flipped");
+    cartesRetournees.push(carte);
+
+    if (cartesRetournees.length === 2) {
+      const [carte1, carte2] = cartesRetournees;
+
+      if (carte1.querySelector(".back img").src === carte2.querySelector(".back img").src) {
+        cartesRetournees = [];
+      } else {
+        setTimeout(() => {
+          carte1.classList.remove("flipped");
+          carte2.classList.remove("flipped");
+          cartesRetournees = [];
+        }, 1000);
+      }
+    }
+  });
+});
